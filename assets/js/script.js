5552e8264c5aeac49cbc38f589d96ed47d543c9a
Luke Thompson <16175583curtin@gmail.com>
2021-06-27 16:52:50 +0800

    basic quiz working

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ var answer1El = document.getElementById('1')
 var answer2El = document.getElementById('2')
 var answer3El = document.getElementById('3')
 var answer4El = document.getElementById('4')
+var containerEl = document.getElementById('container')
+var scoreEl = document.getElementById('score')
 startButtonEl.addEventListener('click', startGame)
 
 function startGame() {
@@ -23,13 +25,76 @@ function questions1() {
     answer2El.innerText = question1.answer2;
     answer3El.innerText = question1.answer3;
     answer4El.innerText = question1.answer4;
+    answer1El.addEventListener('click', function(){
+
+        ++score;
+        questions2();
+    });
+    answer2El.addEventListener('click', function(){
+
+        
+        questions2();
+    });
+    answer3El.addEventListener('click', function(){
+
+        
+        questions2();
+    });
+    answer4El.addEventListener('click', function(){
+
+        
+        questions2();
+    });
 }
 var question1 = {
     question: "What is this",
-    answer1: "Thing1",
+    answer1: "Thing1Correct",
     answer2: "Thing2",
     answer3: "Thing3",
     answer4: "Thing4"
 }
 
-console.log (question1.answer4);
+function questions2() {
+    console.log("something")
+    questionEl.innerText = question2.question;
+    answer1El.innerText = question2.answer1;
+    answer2El.innerText = question2.answer2;
+    answer3El.innerText = question2.answer3;
+    answer4El.innerText = question2.answer4;
+    answer1El.addEventListener('click', function(){
+
+        
+        final();
+    });
+    answer2El.addEventListener('click', function(){
+
+        
+        final();
+    });
+    answer3El.addEventListener('click', function(){
+
+        ++score;
+        final();
+    });
+    answer4El.addEventListener('click', function(){
+
+        
+        final();
+    });
+}
+
+
+var question2 = {
+    question: "Whatsdfasdf is this",
+    answer1: "Thinsdfsg1",
+    answer2: "Tsdfsdfhing2Correct",
+    answer3: "Thsdfsing3",
+    answer4: "Thsdfsdfing4"
+}
+
+function final() {
+
+    questionsContainerEl.classList.add('hide');
+    scoreEl.innerText = ("You scored " + score + " points!")   
+
+}
